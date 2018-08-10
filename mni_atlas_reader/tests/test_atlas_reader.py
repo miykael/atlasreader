01b36090b20396fea3e6cc7c7e5e0d158f6f8f50
Dan Gale <danjgale@gmail.com>
2018-08-09 18:42:08 -0700

    added test_create_output and configured to use a temporary directory for file generation

--- mni_atlas_reader/tests/test_atlas_reader.py
+++ mni_atlas_reader/tests/test_atlas_reader.py
@@ -1,8 +1,29 @@
+import os
 import numpy as np
 from mni_atlas_reader import atlas_reader
-
+from nilearn.datasets import fetch_neurovault_motor_task
 
 def test_get_vox_coord():
     affine = np.eye(4)
     coords = (10, 10, 10)
     assert atlas_reader.get_vox_coord(affine, coords) == [10, 10, 10]
+
+
+def test_create_output(tmpdir):
+
+    motor_images = fetch_neurovault_motor_task()
+    stat_img = motor_images.images[0]
+    stat_img_name = os.path.basename(stat_img)[:-7]
+
+    output_dir = tmpdir.mkdir('mni_test')
+    atlas_reader.create_output(stat_img, atlas=['Harvard_Oxford'],
+                               outDir=output_dir)
+
+    assert os.path.exists(output_dir)
+    assert len(os.listdir(output_dir)) > 0
+    assert os.path.isfile(
+        os.path.join(output_dir, '{}.csv'.format(stat_img_name))
+    )
+    assert os.path.isfile(
+        os.path.join(output_dir, '{}.png'.format(stat_img_name))
+    )
