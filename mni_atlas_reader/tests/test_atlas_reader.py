f2cfc8b854cd4a25b8cbf9a1c59b55dc58479036
Dan Gale <danjgale@gmail.com>
2018-08-09 18:43:42 -0700

    added comments for clarity

--- mni_atlas_reader/tests/test_atlas_reader.py
+++ mni_atlas_reader/tests/test_atlas_reader.py
@@ -11,14 +11,19 @@ def test_get_vox_coord():
 
 def test_create_output(tmpdir):
 
+    # create mock data
     motor_images = fetch_neurovault_motor_task()
     stat_img = motor_images.images[0]
     stat_img_name = os.path.basename(stat_img)[:-7]
 
+    # temporary output
     output_dir = tmpdir.mkdir('mni_test')
+
+
     atlas_reader.create_output(stat_img, atlas=['Harvard_Oxford'],
                                outDir=output_dir)
 
+    # test if output exists and if the key .csv and .png files were created
     assert os.path.exists(output_dir)
     assert len(os.listdir(output_dir)) > 0
     assert os.path.isfile(
