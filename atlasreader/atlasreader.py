1587e74bc17bafb5135b544253b30cfb190831e1
miykael <michaelnotter@hotmail.com>
2018-08-31 17:22:14 +0200

    FIX: corrects previous detection of empty images

--- atlasreader/atlasreader.py
+++ atlasreader/atlasreader.py
@@ -417,7 +417,7 @@ def process_img(stat_img, voxel_thresh=1.96, cluster_extent=20):
             data[data > 0] = 0  # keep only negatives
 
         # Do nothing if data array contains only zeros
-        if np.count_nonzero(data) == 1:
+        if np.any(data):
             clusters += [connected_regions(
                 image.new_img_like(thresh_img, data),
                 min_region_size=min_region_size,
@@ -671,7 +671,7 @@ def create_output(filename, atlas='all', voxel_thresh=1.96, cluster_extent=20,
                                   output_file=glass_fname)
 
     # Check if thresholded image contains only zeros
-    if np.count_nonzero(thresh_img.get_data()) == 1:
+    if np.any(thresh_img.get_data()):
 
         # get cluster + peak information from image
         clust_frame, peaks_frame = get_statmap_info(
