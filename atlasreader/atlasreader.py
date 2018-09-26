77ae56abb3dba3e54e50ba1a3f8b430613e94f4e
miykael <michaelnotter@hotmail.com>
2018-09-26 12:58:35 +0200

    ENH: restrict voxelID to atlas volume

--- atlasreader/atlasreader.py
+++ atlasreader/atlasreader.py
@@ -272,6 +272,37 @@ def get_subpeak_coords(clust_img, min_distance=20):
     return coords
 
 
+def check_atlas_bounding_box(voxIDs, box_shape):
+    """
+    Returns the provided voxel ID if the voxel is inside the bounding box of the
+    atlas image, otherwise the voxel ID will be replaced with the origin.
+
+    Parameters
+    ----------
+    voxIDs : (N, 3) numpy.ndarray
+        `coords` in cartesian space
+    box_shape : (3,) list of int
+        size of the atlas bounding box
+    prob_thresh : [0, 100] int, optional
+        Probability (percentage) threshold to apply if `atlastype` is
+        probabilistic
+
+    Returns
+    ------
+    ijk : (N, 3) numpy.ndarray
+        `coords` in cartesian space that are inside the bounding box
+    """
+
+    # Detect voxels that are outside the atlas bounding box
+    vox_outside_box = np.sum(
+        (voxIDs < 0) + (voxIDs >= box_shape[:3]), axis=-1, dtype='bool')
+
+    # Set those voxels to the origin (i.e. a voxel outside the brain)
+    voxIDs[vox_outside_box] = np.zeros(3, dtype='int')
+
+    return voxIDs
+
+
 def read_atlas_peak(atlastype, coordinate, prob_thresh=5):
     """
     Returns label of `coordinate` from corresponding `atlastype`
@@ -304,6 +335,7 @@ def read_atlas_peak(atlastype, coordinate, prob_thresh=5):
 
     # get voxel index
     voxID = coord_xyz_to_ijk(atlastype.image.affine, coordinate).squeeze()
+    voxID = check_atlas_bounding_box(voxID, data.shape)
 
     # get label information
     # probabilistic atlas is requested
@@ -366,6 +398,7 @@ def read_atlas_cluster(atlastype, cluster, affine, prob_thresh=5):
 
     # get voxel indexes
     voxIDs = coord_xyz_to_ijk(atlastype.image.affine, coords)
+    voxIDs = check_atlas_bounding_box(voxIDs, data.shape)
     voxIDs = tuple(map(tuple, voxIDs.T))
 
     # get label information
