5f12d4085a21f8eabdb8da7bea79fa7596482d99
miykael <michaelnotter@hotmail.com>
2018-10-16 00:47:39 +0200

    FIX: satisfy flake8

--- atlasreader/tests/test_atlasreader.py
+++ atlasreader/tests/test_atlasreader.py
@@ -168,13 +168,11 @@ def test_table_output(tmpdir):
                               outdir=output_dir)
 
     # test if output tables contain expected output
-    cluster_checksum = hashlib.md5(
-        open(output_dir.join('{}_clusters.csv'.format(stat_img_name)),
-        'rb').read()).hexdigest()
+    cluster_checksum = hashlib.md5(open(output_dir.join(
+        '{}_clusters.csv'.format(stat_img_name)), 'rb').read()).hexdigest()
 
-    peak_checksum = hashlib.md5(
-        open(output_dir.join('{}_peaks.csv'.format(stat_img_name)),
-        'rb').read()).hexdigest()
+    peak_checksum = hashlib.md5(open(output_dir.join(
+        '{}_peaks.csv'.format(stat_img_name)), 'rb').read()).hexdigest()
 
     assert cluster_checksum == '5d85805d58f8fbe22ef4e34ec75d8f42'
     assert peak_checksum == 'f7cd664571413fe964eef0c45cd6f033'
