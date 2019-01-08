c4c73b6024291a72728daea4f154ade8778cc7f8
miykael <michaelnotter@hotmail.com>
2019-01-08 15:24:56 +0100

    ENH: add license message to first import in script

--- atlasreader/__init__.py
+++ atlasreader/__init__.py
@@ -2,3 +2,14 @@ __all__ = ['__version__', 'create_output', 'get_statmap_info']
 
 from atlasreader.info import __version__
 from atlasreader.atlasreader import create_output, get_statmap_info
+
+_LICENSE_MESSAGE = """\
+The Python package you are importing, `atlasreader`, is licensed under the
+BSD-3 license; however, the atlases it uses are separately licensed under more
+restrictive frameworks.
+By using `atlasreader`, you agree to abide by the license terms of the
+individual atlases. Information on these terms can be found online at:
+https://github.com/miykael/atlasreader/tree/master/atlasreader/data
+"""
+
+print(_LICENSE_MESSAGE)
